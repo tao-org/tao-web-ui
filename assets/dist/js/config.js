d0b19a13eb0ca0bbd71c37d5c6280cbfb9677042
Narcis PAUN <npaun@c-s.ro>
2017-11-27 17:48:24 +0200

    exposed config variables to config.js file

---
+++ assets/dist/js/config.js
@@ -0,0 +1,8 @@
+var baseURL = '/_tao/';
+var baseRestURL = './_rest/'; //mock html files
+var baseRestURLPHP = 'http://192.168.61.101/_rest/';
+var baseRestApiURL = 'http://localhost:8080/';
+var prefferences = {
+    "viewMode": "grid",
+    "autorefresh": "on"
+};

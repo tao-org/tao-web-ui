b946c341e8c9e047fb7abba113cf9ea833ffb856
Narcis PAUN <npaun@c-s.ro>
2018-04-11 16:34:56 +0300

    component editor

--- assets/dist/js/config.js
+++ assets/dist/js/config.js
@@ -3,9 +3,9 @@ var baseRestURL = './_rest/'; //mock html files
 var baseRestURLPHP = 'http://192.168.61.101/_rest/';
 
 //localhost API
-//var baseRestApiURL = 'http://localhost:8080/';
+var baseRestApiURL = 'http://localhost:8080/';
 //live API
-var baseRestApiURL = 'http://master.tao.c-s.ro:8080/';
+//var baseRestApiURL = 'http://master.tao.c-s.ro:8080/';
 
 var prefferences = {
     "viewMode": "grid",

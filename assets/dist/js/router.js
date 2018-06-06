5f19fad59d7ceceda0178c83117949fb4ca643c4
Narcis PAUN <npaun@c-s.ro>
2018-06-06 18:52:12 +0300

    menu redesign

--- assets/dist/js/router.js
+++ assets/dist/js/router.js
@@ -1,43 +1,74 @@
-/*!
- * Core functions for general page functionality.
- */
+/**
+ * Copyright  CS ROMANIA, http://c-s.ro
+ *
+ * Licensed under the ....;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * file details: core functions and general functionality, request router for single page app.
+ *
+ **/
+
 //default page
-var DEFAULT_PAGE_URL = "#admin/dashboard";
 var taoDynPageDiv = $(".tao-async-cw");
+var taoLoadingDiv = $("#myModalLoading");
 var routeTags = [];
-var hashRoutesMap = [];
-    hashRoutesMap["admin/dashboard"] = "./fragments/dashboard-admin.fragment.html";
-    hashRoutesMap["projects"] = '';
-    hashRoutesMap["workflow/all"] = "./fragments/workflow-all.fragment.html";
-    hashRoutesMap["admin/users"] = "./fragments/users-admin.fragment.html";
-    hashRoutesMap["admin/datasources"] = "./fragments/datasource-admin.fragment.html";
-    hashRoutesMap["admin/topology"] = "./fragments/topology-admin.fragment.html";
-    hashRoutesMap["admin/modules"] = "./fragments/component-admin.fragment.html";
-    hashRoutesMap["documentation/intro"] = "./fragments/tao-documentation.fragment.html";
-    hashRoutesMap["faq/intro"] = "./fragments/tao-faq.fragment.html";
-
-var userOpStack = [];
-
+var routeFilters = {};
+var userOpStack = [];  //holder for aditional user action parameters
+//UI loading indicator management
 function routeLoading(action){
     if(action === "show"){
-        $("#myModalLoading").modal('show');
+        taoLoadingDiv.modal('show');
     }
     if(action === "hide"){
         setTimeout(function(){
-            $("#myModalLoading").modal('hide');
-        },500);
+            taoLoadingDiv.modal('hide');
+        },1000);
     }
 }
 
 //ROUTER
 $(function () {
+    var DEFAULT_PAGE_URL = "#admin/dashboard";
+    var UNDEFINED_PAGE_URL = "#undefined/page";
+    var hashRoutesMap = [];
+    hashRoutesMap["admin/dashboard"] = "./fragments/dashboard-admin.fragment.html";
+    hashRoutesMap["projects"] = '';
+
+    hashRoutesMap["admin/users"] = "./fragments/users-admin.fragment.html";
+    hashRoutesMap["admin/datasources"] = "./fragments/datasource-admin.fragment.html";
+    hashRoutesMap["admin/topology"] = "./fragments/topology-admin.fragment.html";
+    hashRoutesMap["admin/modules"] = "./fragments/component-admin.fragment.html";
+
+    hashRoutesMap["documentation/intro"] = "./fragments/tao-documentation.fragment.html";
+    hashRoutesMap["faq/intro"] = "./fragments/tao-faq.fragment.html";
+
+    hashRoutesMap["my/workflows"] = "./fragments/workflows-all.fragment.html";
+    hashRoutesMap["my/components"] = "./fragments/component-admin.fragment.html";
+    hashRoutesMap["my/auxfiles"] = "./fragments/my-auxfiles.fragment.html";
+    hashRoutesMap["my/scripts"] = "./fragments/my-scripts.fragment.html";
+
+    hashRoutesMap["shared/workflows"] = "./fragments/workflows-all.fragment.html";
+    hashRoutesMap["shared/components"] = "./fragments/component-admin.fragment.html";
+    hashRoutesMap["shared/datasources"] = "./fragments/datasource-admin.fragment.html";
+
+    hashRoutesMap["undefined/page"] = "./fragments/tao-faq.fragment.html";
+
     // Event handlers for frontend navigation
     // An event handler witch calls the router function on every hashchange.
     $(window).on('hashchange', function(){
         //check authtoken
         var tokenKey = _settings.readCookie("tokenKey");
         //var tokenKey = _settings.readCookie("xauthtoken");
-        if ((tokenKey=="") || (tokenKey == null)){
+        if ((tokenKey==="") || (tokenKey == null)){
             $("body").empty();
             window.location = 'login.html';
         }else{
@@ -50,37 +81,40 @@ $(function () {
 
     // Navigation/router function will call appropriate functions.
     function navRouter(hash) {
-        var fragmentUrl = "",
-            unsolvedRoute = true;
+        var fragmentUrl = "";
+        var unsolvedRoute = true;
         if (!hash || (typeof hash === "string" && hash === "") || (typeof hash === "string" && hash === "#")){
             hash = DEFAULT_PAGE_URL;
-        }else{
-            if( hash.charAt(0)==="#") hash = hash.substring(1);
-            routeTags = hash.split('/');
         }
-
-        //fast map resolver
+        if( hash.charAt(0)==="#"){
+            hash = hash.substring(1);
+        }
         if(hashRoutesMap[hash] === undefined){
-        }else{
-            routeLoading('show');
-            fragmentUrl = hashRoutesMap[hash];
-            unsolvedRoute = false;
-            $.ajax({ cache: false,
-                url: fragmentUrl
-            })
-                .done(function (data) {
-                    taoDynPageDiv.html(data);
-                    routeLoading('hide');
-                })
-                .fail(function (jqXHR, textStatus) {
-                    routeLoading('hide');
-                    alert("Could not load page content... Try later.")
-                });
+            hash = UNDEFINED_PAGE_URL;
+            hash = hash.substring(1);
         }
+        routeTags = hash.split('/');
+
+        //map resolver, fragment loader & renderer
+        routeLoading('show');
+        fragmentUrl = hashRoutesMap[hash];
+        unsolvedRoute = false;
+        $.ajax({ cache: false,
+                url: fragmentUrl
+        })
+        .done(function (data) {
+                taoDynPageDiv.html(data);
+                routeLoading('hide');
+        })
+        .fail(function (jqXHR, textStatus) {
+                taoDynPageDiv.html('');
+                routeLoading('hide');
+                alert("Could not load page content... Try later.")
+        });
         // Sync active menu. Remove active from navigation, and generically mark branch as active.
         $('.sidebar-menu .routed').removeClass('active');
         $('.sidebar-menu .routed-'+routeTags[0]+'-'+routeTags[1]).addClass("active");
-        console.log('.sidebar-menu .routed-'+routeTags[0]+'-'+routeTags[1]);
+        //console.log('.sidebar-menu .routed-'+routeTags[0]+'-'+routeTags[1]);
 
         if(routeTags[0] === "account"){
             if(routeTags[1] === "profile"){
@@ -102,26 +136,3 @@ $(function () {
     //trigger a hashchange to start app
     $(window).trigger('hashchange');
 });
-
-//onMessage to enable iframe/parent comunnication
-if (window.addEventListener) {
-    window.addEventListener("message", onMessage, false);
-}
-else if (window.attachEvent) {
-    window.attachEvent("onmessage", onMessage, false);
-}
-
-function onMessage(event) {
-    // Check sender origin to be trusted
-    if (event.origin !== "http://example.com") return;
-
-    var data = event.data;
-
-    if (typeof(window[data.func]) == "function") {
-        window[data.func].call(null, data.message);
-    }
-}
-// Function to be called from iframe
-function parentFunc(message) {
-    alert(message);
-}

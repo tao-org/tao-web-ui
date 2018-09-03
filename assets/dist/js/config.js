a59e71b0efae5c607c5afc4687e531787c2127e2
Narcis PAUN <npaun@c-s.ro>
2018-09-03 16:39:54 +0300

    remove test server references and mock api for testing

--- assets/dist/js/config.js
+++ assets/dist/js/config.js
@@ -1,7 +1,3 @@
-var baseURL = '/_tao/';
-var baseRestURL = './_rest/'; //mock html files
-var baseRestURLPHP = 'http://192.168.61.101/_rest/';
-
 //localhost API
 var baseRestApiURL = 'http://localhost:8080/';
 //live API
@@ -26,14 +22,4 @@ var responseStatus = {
 //msg dictionary
 var SHARED_EDIT_RESTRICTED = "Users can not edit shared components. User have edit permission restricted to own workspace.";
 
-
-//xhr.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + password));
-/*
-headers: {
-"Accept": "application/json",
-"Content-Type": "application/json",
-"X-Auth-Token": window.tokenKey
-"Authorization": "Basic " + btoa("admin:admin")
-}
-*/
 
